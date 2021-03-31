0472de016c84887a26bfcb34c509959834867cb3
Walter Smith <walter.c.smith01@gmail.com>
2021-03-31 14:19:28 -0400

    Feat:Added getParameter to reset count

--- src/main/java/PageViewCounter.java
+++ src/main/java/PageViewCounter.java
@@ -14,7 +14,7 @@ public class PageViewCounter extends HttpServlet{
     }
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException{
 
         String reset = request.getParameter("reset");
 
