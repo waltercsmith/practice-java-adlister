ee9b8525dedc95d24b8814d4bcdf7bd97a90866d
Walter Smith <walter.c.smith01@gmail.com>
2021-03-31 13:21:42 -0400

    Feat:Refactored code and included an if statement under the doGet method if the user enters an input as part of the query string, then th page will display Hello, and whatever the user entered. Otherise the page will display simply Hello Word

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,15 +6,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+
+
+        String userInput = req.getParameter("input");
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!<h1>");
+        if (userInput != null) {
+            PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, World!<h1>");
+            out.printf("<h1>Hello, %s</h1>", userInput); // <--- when the user types ?input= the page will display Hello, and what every that was typed
+        } else {
+            PrintWriter out1 = response.getWriter();
+            out1.println("<h1>Hello, World</h1>");
+        }
 
     }
+
 }
